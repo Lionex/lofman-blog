f475b47fc0ba57d1d8c686247ef1177d69dcadff
Gwen Lofman <gwen@lofman.co>
2016-07-01 03:08:02 -0400

    Added PORT parsing
    Reads $PORT and passes the port to Spock, fixing failure to bind to
    Heroku Port.

--- Main.hs
+++ Main.hs
@@ -5,10 +5,13 @@ module Main where
 import           Control.Monad.Cont
 import           Data.Monoid
 import qualified Data.Text.Lazy                as TL
+import           System.Environment
 import qualified Text.Blaze.Html               as H
 import           Text.Blaze.Html.Renderer.Text (renderHtml)
 import qualified Text.Blaze.Html5              as H
 import           Web.Spock.Safe
+
+
 
 
 helloSpock :: SpockAction db session state ()
@@ -20,8 +23,11 @@ app = do get "/" helloSpock
 
 
 main :: IO ()
-main = runSpock 8080 $ spockT id $
-    do get root $
-           text "Hello World!"
-       get ("post" <//> var) $ \postId ->
-           text ("Not implemented, unable to get " <> postId <> " from database.")
+main = do
+    -- Parse the port from Heroku $PORT variable
+    port <- liftM read (getEnv "PORT")
+
+    -- Run the web app
+    runSpock port $ spockT id $
+        do get root $ text "Hello World!"
+           get ("post" <//> var) $ \postId -> text ("Hello" <> postId)
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: /app/.local/bin/lofman-blog -p 8080 
+web: /app/.local/bin/lofman-blog
