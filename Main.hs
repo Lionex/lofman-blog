d4c1143401b3297c687c5229ce2e168e7f264b8a
Gwen Lofman <gwen@lofman.co>
2016-07-02 18:25:21 -0400

    Added support for Blaze HTML.
    Wrote function `blaze` to render to text for Spock.

--- Main.hs
+++ Main.hs
@@ -1,33 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
-
 import           Control.Monad.Cont
 import           Data.Monoid
 import qualified Data.Text.Lazy                as TL
 import           System.Environment
 import qualified Text.Blaze.Html               as H
-import           Text.Blaze.Html.Renderer.Text (renderHtml)
+import           Text.Blaze.Html.Renderer.Utf8 (renderHtml)
 import qualified Text.Blaze.Html5              as H
 import           Web.Spock.Safe
+import           Web.Template
+
+
+blaze :: (MonadIO m) => H.Html -> ActionCtxT ctx m a
+blaze = lazyBytes . renderHtml
 
 
 
-
-helloSpock :: SpockAction db session state ()
-helloSpock = do html "Hello, <em> Spock </em>"
-
-
-app :: SpockM database session state ()
-app = do get "/" helloSpock
-
+--The Main Function
+-------------------
 
 main :: IO ()
 main = do
-    -- Parse the port from Heroku $PORT variable
-    port <- liftM read (getEnv "PORT")
-
-    -- Run the web app
-    runSpock port $ spockT id $
-        do get root $ text "Hello World!"
-           get ("post" <//> var) $ \postId -> text ("Hello" <> postId)
+  port <- liftM read (getEnv "PORT")
+  runSpock port $ spockT id $
+    do get root $ blaze $ wrapper "Lofman.co" (H.meta) (H.h1 "Hello World!")
+       get ("post" <//> var) $ \postId ->
+         text ("Hello " <> postId)
--- Server/App.hs
+++
@@ -1,9 +0,0 @@
-module LofmanBlog.Server where
-
-
--- | The routing and application logic of the serer.
---
--- Links each route-pattern to a SpockAction, and runs any other necessary
--- actions.
-app :: IO ()
-app = putStrLn "Not implemented"
