a1cf3f464fc11062b4328b7e8407dcbb1e81dc37
Gwen Lofman <gwen@lofman.co>
2016-07-04 23:43:14 -0400

    Began defining core database types.

---
+++ Src/Model/Types.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+module Model.Types where
+
+import           Control.Monad.IO.Class (liftIO)
+import           Database.Persist
+import           Database.Persist.Sql
+import           Database.Persist.TH
+
+
+share [mkPersist sqlSettings, mkSave "entityDefs"] [persistLowerCase|
+
+BlogPost
+    title            String
+    project          ProjectId
+    postDate         String
+    content          String
+    deriving         Show
+
+Author
+    fname            String
+    lname            String
+    profiles         String
+    deriving         Show
+
+Project
+    name             String
+    description      String
+    deriving         Show
+
+BlogAuthor
+    blogPostId       BlogPostId
+    authorId         AuthorId
+    UniqueBlogAuthor blogPostId authorId
+    deriving         Show
+
+|]
