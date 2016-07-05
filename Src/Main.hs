d69ecf71461889b836e4187b6839c7eee8e9cd22
Gwen Lofman <gwen@lofman.co>
2016-07-04 23:41:48 -0400

    Using new view code to generate web page.

--- Src/Main.hs
+++ Src/Main.hs
@@ -1,29 +1,34 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
+
 import           Control.Monad.Cont
 import           Data.Monoid
 import qualified Data.Text.Lazy                as TL
+import           Model.Types
 import           System.Environment
 import qualified Text.Blaze.Html               as H
 import           Text.Blaze.Html.Renderer.Utf8 (renderHtml)
 import qualified Text.Blaze.Html5              as H
 import           Web.Spock.Safe
-import           Web.Template
-
+import           Web.View.Meta
+import           Web.View.Template
 
 blaze :: (MonadIO m) => H.Html -> ActionCtxT ctx m a
 blaze = lazyBytes . renderHtml
 
-
-
---The Main Function
--------------------
-
 main :: IO ()
 main = do
-  port <- liftM read (getEnv "PORT")
-  runSpock port $ spockT id $
-    do get root $ blaze $ wrapper "Lofman.co" (H.meta) (H.h1 "Hello World!")
-       get ("post" <//> var) $ \postId ->
-         text ("Hello " <> postId)
+    port <- liftM read (getEnv "PORT")
+    runSpock port $ spockT id $ app
+
+app :: SpockCtxT ctx IO ()
+app = do
+    get root $
+        let meta_ = twitterCard "Art + Technology" "@gwelof" "Gwen Lofman's web page"
+        in blaze $ wrapper "Lofman.co" meta_ (H.h1 "Hello World!")
+
+    get ("post" <//> var) $ \postId ->
+        let title = H.toHtml (postId :: TL.Text)
+            meta_ = twitterCard "Yay" "@gwelof" "Gwen Lofman's web page"
+        in blaze $ wrapper title meta_ (H.h1 title)
