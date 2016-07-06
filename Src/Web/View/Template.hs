fca83d421024aa53d698b504470bcad98a903e28
Gwen Lofman <gwen@lofman.co>
2016-07-06 11:48:30 -0400

    Updated structure of page.
    Wrapper now accommodates new method for handling meta-data; site Navigation
    now uses Icon as home page link, updated html class attributes to better
    reflect content.

--- Src/Web/View/Template.hs
+++ Src/Web/View/Template.hs
@@ -9,32 +9,30 @@ import qualified Text.Blaze.Html             as H
 import           Text.Blaze.Html5            ((!))
 import qualified Text.Blaze.Html5            as H
 import           Text.Blaze.Html5.Attributes as A
-import           Web.View.Meta
 import           Web.View.Util
 
-wrapper :: H.Html -> [H.Html] -> H.Html -> H.Html
-wrapper title meta_ body = H.docTypeHtml $ do
+wrapper :: H.Html -> H.Html -> H.Html
+wrapper meta body = H.docTypeHtml $ do
     H.head $ do
-        H.title title
         H.meta ! A.charset "utf-8"
-        forM_ meta_ Prelude.id
+        meta
     H.body $ do
-        siteNav nav
-        H.main ! A.class_ "content" $ do body
-        footer [H.a "Facebook"] nav
-    where nav = [H.a "home" ! A.href "/"]
+        siteNav []
+        H.main ! A.class_ "content" $ body
+        footer [H.a "Facebook" ! A.href "https://www.facebook.com/gwen.lofman"] []
 
 siteNav :: [H.Html] -> H.Html
 siteNav nav = H.header ! A.class_ "siteHead" $ do
-    H.nav $ do
-        H.span $ H.img ! A.src siteIcon
-        H.ul $ forM_ nav (H.li)
+    H.nav ! A.class_ "crossfade" $ do
+        H.ul ! A.class_ "nav" $ do
+            H.span $ H.a ! A.href "/" $ H.img ! A.src siteIcon
+            forM_ nav (H.li ! A.class_ "nav-button")
 
 footer :: [H.Html] -> [H.Html] -> H.Html
 footer social nav = H.footer ! A.class_ "site" $ do
-    H.article ! A.class_ "profile" $ do
+    H.aside ! A.class_ "profile" $ do
         H.h2 "Gwen Lofman"
         H.ul ! A.class_ "social" $ forM_ social (H.li)
 
-    H.nav $ do
-        H.ul ! A.class_ "nav" $ forM_ nav (H.li)
+    H.nav  $ do
+        H.ul ! A.class_ "nav nav-footer" $ forM_ nav (H.li)
