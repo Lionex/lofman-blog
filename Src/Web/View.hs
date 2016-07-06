22001f39d1ff81eaae264ca4dbbfe2a8f80bde50
Gwen Lofman <gwen@lofman.co>
2016-07-06 15:54:15 -0400

    Created new instance declarations.
    Added declaration for Maybe a

--- Src/Web/View.hs
+++ Src/Web/View.hs
@@ -9,10 +9,21 @@ import           Text.Blaze.Html5            ((!))
 import           Text.Blaze.Html5            as H
 import           Text.Blaze.Html5.Attributes as A
 
+-- |
 class View a where
-    toBody :: a -> H.Html
+    toBody  :: a -> H.Html
+    toTitle :: a -> H.Html
+
+instance View a => View (Maybe a) where
+    toBody Nothing   = ""
+    toBody (Just a)  = toBody a
+
+    toTitle Nothing  = ""
+    toTitle (Just a) = toTitle a
 
 instance View Page where
     toBody Home = do
         H.h1 "Hello World!"
         H.p "This site is currently under construction.  Please come back soon!"
+
+    toTitle Home = H.title "Lofman.co"
