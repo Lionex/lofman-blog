6edf4a1f5952c3df89903815f15e953f4f4a7223
Gwen Lofman <gwen@lofman.co>
2016-07-10 20:22:19 -0400

    Added instance for

--- Src/Web/View.hs
+++ Src/Web/View.hs
@@ -6,14 +6,18 @@ module Web.View ( View(..)
 
 import           Control.Monad               (forM_)
 import           Model.Types
+import           Model.DbTypes
 import           Text.Blaze.Html5            (Html (..), (!))
-import           Text.Blaze.Html5            as H
+import qualified Text.Blaze.Html5            as H
 import           Text.Blaze.Html5.Attributes as A
+import           Web.View.Util
 
 -- | Determines how to render data into HTML.
 class View a where
     toBody  :: a -> Html -- ^ Renders data into content.
+
     toTitle :: a -> Html -- ^ Renders data into page title.
+    toTitle _ = "Lofman.co"
 
 instance View a => View (Maybe a) where
     toBody Nothing   = H.h1 "something went wrong"
@@ -23,7 +27,7 @@ instance View a => View (Maybe a) where
     toTitle (Just a) = toTitle a
 
 instance View a => View [a] where
-    toBody xs        = forM_ xs ((H.div ! A.class_ "container") . toBody)
+    toBody xs        = H.div ! A.class_ "container" $ forM_ xs (toBody)
 
     toTitle []       = toTitle Home
     toTitle (x:xs)   = toTitle x
@@ -31,6 +35,11 @@ instance View a => View [a] where
 instance View Page where
     toBody Home      = do
         H.h1 "Hello World!"
-        H.p "This site is currently under construction.  Please come back soon!"
+        H.p  "This site is currently under construction.  Please come back soon!"
+    toBody Error404  = do
+        H.h1 "Error 404"
+        H.p $ do "Something went wrong, go"
+                 link "home" "/"
 
     toTitle Home     = H.title "Lofman.co"
+    toTitle Error404 = H.title "Lofman.co: Error, not found."
