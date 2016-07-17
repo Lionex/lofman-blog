41c0341e996124be77efc84b0ad9727eb60cc173
Gwen Lofman <gwen@lofman.co>
2016-07-17 13:13:10 -0400

    Merge of branch feature-static-files

--- Src/Main.hs
+++ Src/Main.hs
@@ -4,13 +4,21 @@ module Main where
 
 import           Control.Monad.Cont
 import           System.Environment
+import           Network.Wai
+import           Network.Wai.Middleware.Static
+import           Network.Wai.Middleware.RequestLogger
 import qualified Web.Handler        as Handler
 import           Web.Spock.Safe
 
 main :: IO ()
 main = do
     port <- liftM read (getEnv "PORT")
-    runSpock port $ spockT id $ app
+    runSpock port $ spockT id $ appMiddleware app
+
+appMiddleware :: SpockT IO ()
+appMiddleware = do
+  middleware logStdoutDev
+  middleware $ staticPolicy (noDots >-> addBase "static")
 
 app :: SpockCtxT ctx IO ()
 app = do
