0a0c3a290668bd9e3c707cc16dd564761c3ac19f
Gwen Lofman <gwen@lofman.co>
2016-07-17 20:41:28 -0400

    Adds database connection capabilities.
    Reads database connection parameters, parses them and pases them to
    persistent ORM. Adds bytestring package dependency and import to parse database
    connection parameters.

--- Src/Main.hs
+++ Src/Main.hs
@@ -2,26 +2,32 @@
 
 module Main where
 
-import           Clay                          ( Css(..)
-                                               , compact
-                                               , pretty
-                                               , renderWith
-                                               )
+import           Clay                                ( Css(..)
+                                                     , compact
+                                                     , pretty
+                                                     , renderWith
+                                                     )
 import           Control.Monad.Cont
+import           Control.Monad.Logger                (runStderrLoggingT)
+import qualified Data.ByteString                     as B
 import           Data.Monoid
-import qualified Data.Text.Lazy                as TL
-import qualified Data.Text.Lazy.Encoding       as TL
+import qualified Data.Text.Lazy                      as TL
+import qualified Data.Text.Lazy.Encoding             as TL
+import qualified Data.Text.Encoding                  as T
+import           Database.Persist            hiding (get)
+import           Database.Persist.Postgresql hiding (get)
 import           Model.DbTypes
 import           Model.Types
 import           System.Environment
-import qualified Text.Blaze.Html               as H
-import           Text.Blaze.Html.Renderer.Utf8 (renderHtml)
-import qualified Text.Blaze.Html5              as H
+import qualified Text.Blaze.Html                     as H
+import           Text.Blaze.Html.Renderer.Utf8       (renderHtml)
+import qualified Text.Blaze.Html5                    as H
 import           Web.Spock.Safe
 import           Web.View
 import           Web.View.Meta
 import           Web.View.Template
 import           Web.View.Style
+import           Web.Heroku
 
 blaze :: (MonadIO m) => H.Html -> ActionCtxT ctx m a
 blaze = lazyBytes . renderHtml
@@ -31,10 +37,20 @@ clay = H.style . H.toHtml . (renderWith compact [])
 
 main :: IO ()
 main = do
+    -- Get parameters from Heroku
     port <- liftM read (getEnv "PORT")
-    runSpock port $ spockT id $ app
+    params <- dbConnParams
+    let conn = B.concat . concat . map (\(a,b) ->
+               T.encodeUtf8 a : T.encodeUtf8 b : [])
 
-app :: SpockCtxT ctx IO ()
+    -- Create database connection pool
+    pool <- runStderrLoggingT $ createPostgresqlPool (conn params) 5
+    runStderrLoggingT $ runSqlPool (runMigration migrateAll) pool
+
+    -- Run app with database conneciton pool
+    runSpock port $ spock (defaultSpockCfg Nothing (PCPool pool) ()) $ app
+
+-- app :: SpockCtxT ctx IO ()
 app = do
     get root $
         let sheet = clay $ toStyle Home
