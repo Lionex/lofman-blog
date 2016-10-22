1bf346a5bf80ad89d699aa99e3382aaad1e6c8cc
Gwen Lofman <gwen@lofman.co>
2016-10-22 12:35:59 -0400

    Defines routes for the API

---
+++ Src/API.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TypeOperators #-}
+
+module API where
+( API
+, api
+)
+
+import           Data.Aeson
+import           Data.Proxy
+import           GHC.Generics
+import           Model
+import           Servant.API
+
+type BlogAPI = "blog"
+    :> Get '[JSON] [BlogPost]
+    :<|> ReqBody '[JSON] BlogPost :> Post '[JSON] BlogPost
+    :<|> Capture "blogPostId" BlogPostId :> Get '[JSON] BlogPost
+
+type ProjectAPI = "project"
+    :> Get '[JSON] [Project]
+    :<|> ReqBody '[JSON] Project :> Post '[JSON] Project
+    :<|> Capture "projectID" ProjectId :> Get '[JSON] Project
+
+type AuthorAPI = "author"
+    :> Get '[JSON] [Author]
+    :<|> ReqBody '[JSON] Author :> Post '[JSON] Author
+    :<|> Capture "authorId" AuthorId :> Get '[JSON] Author
+
+type API' = "api"
+    :> BlogAPI
+    :<|> ProjectAPI
+    :<|> AuthorAPI
+
+type API = API' :<|> Raw
+
+api :: Proxy API
+api = Proxy
