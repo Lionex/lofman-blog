3383f81f2eae9b8d1db6e1f3e7ba6489cc57e050
Gwen Lofman <gwen@lofman.co>
2016-10-23 01:12:52 -0400

    Creates new FromText instances for DB ID types
    Necessary to use DB ID types in URL captures

--- Src/Model/Instances.hs
+++ Src/Model/Instances.hs
@@ -1,18 +1,34 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Model.Instances where
 
 import           Data.Aeson
+import           Database.Persist
+import           Database.Persist.Postgresql
+import           Database.Persist.TH
 import           GHC.Generics
 import           Model.DbTypes
+import           Servant
+
 
 instance ToJSON   BlogPost
 instance FromJSON BlogPost
 
-instance ElmType  Project
+instance FromText (BlogPostId) where
+    fromText = fmap toSqlKey . fromText
+
 instance ToJSON   Project
 instance FromJSON Project
 
+instance FromText (ProjectId) where
+    fromText = fmap toSqlKey . fromText
+
 instance ToJSON   Author
 instance FromJSON Author
+
+instance FromText (AuthorId) where
+    fromText = fmap toSqlKey . fromText
+
 
 instance ToJSON   BlogAuthor
 instance FromJSON BlogAuthor
