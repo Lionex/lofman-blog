6404f44da6bc77964124ffa0b94db1b1a01b9bc4
Gwen Lofman <gwen@lofman.co>
2016-10-24 23:41:35 -0400

    Implements more general CRUD API
    Implements each API in terms of the new CRUD API

--- Src/App/Blog.hs
+++ Src/App/Blog.hs
@@ -3,24 +3,9 @@ module App.Blog
 ) where
 
 import           API
-import           Data.Time
-import           Model
+import           App.Crud
+import           Database.Persist.Postgresql (ConnectionPool)
 import           Servant
 
-blogApp :: Server BlogAPI
-blogApp =
-    getBlogs :<|>
-    getBlog  :<|>
-    postBlog
-
--- getBlogs :: Handler [BlogPost]
-getBlogs = return [post]
-
--- getBlog :: BlogPostId -> Handler BlogPost
-getBlog _ = return post
-
--- postBlog :: BlogPost -> Handler BlogPost
-postBlog _ = return post
-
-post :: BlogPost
-post = BlogPost "title" "Essay" "this is an essay" 124123 12
+blogApp :: ConnectionPool -> Server BlogAPI
+blogApp = crudApp
--- Src/App/Project.hs
+++ Src/App/Project.hs
@@ -3,23 +3,9 @@ module App.Project
 ) where
 
 import           API
-import           Data.Time
-import           Model
+import           App.Crud
+import           Database.Persist.Postgresql (ConnectionPool)
 import           Servant
 
-projectApp :: Server ProjectAPI
-projectApp =
-    getProjects :<|>
-    getProject  :<|>
-    postProject
-
--- getProjects :: Handler [Projects]
-getProjects = return [project]
-
--- getProject :: ProjectId -> Handler Project
-getProject _ = return project
-
--- postProject :: Project -> Handler Project
-postProject _ = return project
-
-project = Project "proj" "desc"
+projectApp :: ConnectionPool -> Server ProjectAPI
+projectApp = crudApp
